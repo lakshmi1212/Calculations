138b6d312364e5f7775328c103ee2f88fb95418e
lakshmi1212 <36476419+lakshmi1212@users.noreply.github.com>
2026-01-09 16:58:50 +0530

    Add or update file: test_add.py

--- tests/test_add.py
+++ tests/test_add.py
@@ -7,12 +7,13 @@ def test_add_positive_numbers():
 def test_add_negative_numbers():
     assert add(-2, -3) == -5
 
-def test_add_positive_and_negative():
-    assert add(5, -3) == 2
+def test_add_mixed_sign_numbers():
+    assert add(-2, 3) == 1
 
 def test_add_zero():
-    assert add(0, 5) == 5
+    assert add(0, 0) == 0
     assert add(5, 0) == 5
+    assert add(0, 5) == 5
 
 def test_add_large_numbers():
-    assert add(123456789, 987654321) == 1111111110
+    assert add(1000000, 999999) == 1999999
