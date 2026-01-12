56c1464675b41a6ed92162d6d302d52a0aa460b9
lakshmi1212 <36476419+lakshmi1212@users.noreply.github.com>
2026-01-12 12:29:24 +0530

    Add or update file: test_subtract.py

--- tests/test_subtract.py
+++ tests/test_subtract.py
@@ -16,5 +16,8 @@ def test_subtract_zero():
     assert subtract(0, 5) == -5
     assert subtract(5, 0) == 5
 
+def test_subtract_floats():
+    assert subtract(5.5, 2.1) == pytest.approx(3.4)
+
 def test_subtract_large_numbers():
-    assert subtract(2000000, 1000000) == 1000000
+    assert subtract(2_000_000, 1_000_000) == 1_000_000
